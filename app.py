94a30efdc65a7a191eeacac427d4d8fced22ffe9
light483-l <imlight483@gmail.com>
2025-05-14 00:54:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ def index():
 
 @app.route('/table_param/<gender>/<int:age>')
 def table_param(gender, age):
-    # Определяем цвет стен
     wall_color = '#FFC0CB' if gender.lower() == 'female' else '#87CEEB'
 
-    # Определяем изображение марсианина
     marsian_img = 'child_marsian.png' if age < 21 else 'adult_marsian.png'
 
     return render_template('table_param.html',
